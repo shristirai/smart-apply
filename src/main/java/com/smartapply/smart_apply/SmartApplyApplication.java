94e930774dcbe3291c778222de1c8df33401d412
Shristi Rai <shristirai848@gmail.com>
2026-06-30 23:51:55 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/smartapply/smart_apply/SmartApplyApplication.java
@@ -0,0 +1,13 @@
+package com.smartapply.smart_apply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartApplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartApplyApplication.class, args);
+	}
+
+}
